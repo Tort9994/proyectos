8a3eb94b52243fda37d0003ecb5185611865c56a
Tort9994 <108017207+Tort9994@users.noreply.github.com>
2022-11-02 20:17:49 -0600

    Add files via upload

---
+++ JS/videoModalOptions.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+  window.addEventListener("DOMContentLoaded", function () {
+    new ModalVideo(".videoModal", {
+      channel: "youtube",
+      youtube: {
+        cc_load_policy: 1,
+        modestbranding: 1,
+        rel: 0,
+        controls: 1,
+        autoplay: 1,
+        allowFullScreen: false,
+      },
+    });
+  });
+});
